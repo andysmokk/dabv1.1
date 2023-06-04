cae6ba33ba5a789930168e9b291fe689cb1de04a
Andrii Konovalov <andy.smokk@gmail.com>
2023-06-04 02:43:52 +0200

    fix readme

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ app.use(
 );
 app.use(express.json());
 
+app.get("/shopping-cart", (req, res) => {
+  // Відправка відповіді на маршрут /shopping-cart
+  res.send("Shopping Cart Page");
+});
+
 app.use("/orders", orderRouter);
 
 app.use((req, res) => {
